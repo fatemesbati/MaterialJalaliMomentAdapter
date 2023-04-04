44d92d4a6869d1c1f5cfbe188b4cb9d5581b4e04
Fateme Esbati <fatemesbati79@gmail.com>
2023-04-04 15:49:28 +0330

    update angular material to 15

--- projects/jalaliAdapterUsage/src/app/app.module.ts
+++ projects/jalaliAdapterUsage/src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatLuxonDateModule } from '@angular/material-luxon-adapter';
-import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MaterialJalaliMomentAdapterModule } from 'material-jalali-moment-adapter';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { MaterialJalaliMomentAdapterModule } from 'material-jalali-moment-adapter';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule } from '@angular/material/button';
-// import { MaterialJalaliMomentAdapterModule } from 'projects/material-jalali-moment-adapter/src/lib/adaptor';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MaterialJalaliMomentAdapterModule } from 'projects/material-jalali-moment-adapter/src/lib/adaptor';
 @NgModule({
   declarations: [
     AppComponent,
